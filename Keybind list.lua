280ebe45b0f74dfa6ea32f00b9a88f5d8be88d27
Muqa1 <106272829+Muqa1@users.noreply.github.com>
2023-03-12 03:16:17 +0200

    Update Keybind list.lua

--- Keybind list.lua
+++ Keybind list.lua
@@ -1,17 +1,24 @@
 local box1_color = {25, 25, 25, 255} -- the box above the coloured line
-local coloured_line = {255, 132, 0, 255} -- the coloured line
+local coloured_line = {255, 190, 94, 255} -- the coloured line
 local keyboard_logo_color = {255, 255, 255, 255} -- the keyboard logo colour 
 local keybinds_color = {255, 255, 255, 255} -- the "keybinds" text colour that is next to the keyboard logo
 local keybind_state_colours = {255, 255, 255, 255} -- the keybind state colour like for an example "doubletap: 100%"
 
 
 -- no need to edit this stuff down here
+-- if you can paste anything out of this spagetti code then feel free to do so
+deafultFont = draw.CreateFont( "Tahoma", 14, 800 )
+KeyboardFont = draw.CreateFont( "untitled-font-1", 24, 400 )
+SpectatorFont = draw.CreateFont( "untitled-font-1", 16, 400 )
 
-REealFont = draw.CreateFont( "Tahoma", 14, 800 )
-IconFont = draw.CreateFont( "untitled-font-1", 24, 400 )
+keybinds_start_x = 10
+keybinds_start_y = 800
 
-menu_x = 200
-menu_y = 200
+spectators_start_x = 10
+spectators_start_y = 650
+
+infopanel_start_x = 1690
+infopanel_start_y = 32
 
 local function waujmujmw()
 if engine.IsGameUIVisible() == false then
@@ -20,8 +27,11 @@ if engine.IsGameUIVisible() == false then
     local mY = input.GetMousePos()[2] -- mouse y position
     local boxWidth, boxHeight = 200, 75 -- size of the box
 
-    draw.SetFont( REealFont ) -- set the font to get the text size
+    draw.SetFont( deafultFont ) -- set the font to get the text size
 
+
+
+    --[[Here starts the keybinds box]]--
     local statuses = {}
 
 --============================--
@@ -30,7 +40,16 @@ if engine.IsGameUIVisible() == false then
     if warp.GetChargedTicks() ~= 0 then -- doubletap
 
         local porcent = math.floor((warp.GetChargedTicks() / 23) * 100)
-        table.insert(statuses, "Doubletap: ".. porcent .. "%")
+
+        local LocalWeapon = entities.GetLocalPlayer():GetPropEntity( "m_hActiveWeapon" )
+
+        if (warp.CanDoubleTap(LocalWeapon)) and ((entities.GetLocalPlayer():GetPropInt( "m_fFlags" )) & FL_ONGROUND) == 1 then 
+            canDt = " (ready)"
+        else
+            canDt = " (not ready)"
+        end
+
+        table.insert(statuses, "Doubletap: ".. porcent .. "%".. canDt)
 
     end
 
@@ -98,7 +117,7 @@ if engine.IsGameUIVisible() == false then
     for i, status in ipairs(statuses) do
         draw.Color( table.unpack(keybind_state_colours) )
         local width, height = draw.GetTextSize(status)
-        draw.Text(math.floor(menu_x + 2), math.floor(menu_y + startY), status)
+        draw.Text(math.floor(keybinds_start_x + 2), math.floor(keybinds_start_y + startY), status)
         startY = startY + 15
         textHeight = textHeight + (height + 1)
     end
@@ -106,28 +125,80 @@ if engine.IsGameUIVisible() == false then
     boxHeight = 20 + textHeight -- update the box height
 
     if input.IsButtonDown(MOUSE_LEFT) and -- is the cursor inside the box?
-       mX >= menu_x and mX <= menu_x + boxWidth and
-       mY >= menu_y and mY <= menu_y + boxHeight then
-        menu_x = mX - boxWidth/2
-        menu_y = mY - boxHeight/2
+       mX >= keybinds_start_x and mX <= keybinds_start_x + boxWidth and
+       mY >= keybinds_start_y and mY <= keybinds_start_y + boxHeight then
+        keybinds_start_x = mX - boxWidth/2
+        keybinds_start_y = mY - boxHeight/2
     end
 
     --draw.Color(50, 50, 50, 150) -- drawing the main box  -- currently disabled because it is drawing on top of the text
-    --draw.FilledRect(math.floor(menu_x), math.floor(menu_y), math.floor(menu_x + boxWidth), math.floor(menu_y + boxHeight))
+    --draw.FilledRect(math.floor(keybinds_start_x), math.floor(keybinds_start_y), math.floor(keybinds_start_x + boxWidth), math.floor(keybinds_start_y + boxHeight))
 
     draw.Color( table.unpack(box1_color) ) -- drawing the darker box above the main box
-    draw.FilledRect(math.floor(menu_x), math.floor(menu_y), math.floor(menu_x + boxWidth), math.floor(menu_y + 20))  
+    draw.FilledRect(math.floor(keybinds_start_x), math.floor(keybinds_start_y), math.floor(keybinds_start_x + boxWidth), math.floor(keybinds_start_y + 20))  
 
     draw.Color( table.unpack(coloured_line) ) -- drawing the coloured line
-    draw.FilledRect(math.floor(menu_x), math.floor(menu_y) + 18, math.floor(menu_x + boxWidth), math.floor(menu_y + 20))
+    draw.FilledRect(math.floor(keybinds_start_x), math.floor(keybinds_start_y) + 18, math.floor(keybinds_start_x + boxWidth), math.floor(keybinds_start_y + 20))
 
-    draw.SetFont( IconFont ) -- drawing the keyboard logo
+    draw.SetFont( KeyboardFont ) -- drawing the keyboard logo
     draw.Color( table.unpack(keyboard_logo_color) )
-    draw.Text(math.floor(menu_x) + 2, math.floor(menu_y) - 2,  "a" )
+    draw.Text(math.floor(keybinds_start_x) + 2, math.floor(keybinds_start_y) - 2,  "a" )
     
-    draw.SetFont( REealFont ) -- drawing the keybinds text
+    draw.SetFont( deafultFont ) -- drawing the keybinds text
     draw.Color(table.unpack(keybinds_color))
-    draw.Text(math.floor(menu_x) + 30, math.floor(menu_y) + 2, "keybinds")
+    draw.Text(math.floor(keybinds_start_x) + 30, math.floor(keybinds_start_y) + 2, "keybinds")
+--[[Here ends the keybinds box]]--
+
+
+--[[Here starts the spectators box]]--
+
+local players = entities.FindByClass("CTFPlayer")
+
+startpos = 5
+for i, p in pairs(players) do
+if (p:IsAlive() == false) and (p:IsDormant() == false) then
+
+    if p:GetPropEntity("m_hObserverTarget"):GetName() == steam.GetPlayerName( steam.GetSteamID() ) then
+        startpos = startpos + 15
+
+        if p:GetPropInt("m_iObserverMode") == 1 then 
+            specMode = "Deathcam"
+        elseif p:GetPropInt("m_iObserverMode") == 2 then 
+            specMode = "Freezecam"
+        elseif p:GetPropInt("m_iObserverMode") == 4 then 
+            specMode = "Firstperson"
+        elseif p:GetPropInt("m_iObserverMode") == 5 then 
+            specMode = "Thirdperson"
+        end
+
+    draw.Text(math.floor(spectators_start_x), math.floor(spectators_start_y + startpos), p:GetName().. ": ".. specMode)
+    end
+
+end
+end
+
+if input.IsButtonDown(MOUSE_LEFT) and -- is the cursor inside the box?
+       mX >= spectators_start_x and mX <= spectators_start_x + boxWidth and
+       mY >= spectators_start_y and mY <= spectators_start_y + boxHeight then
+        spectators_start_x = mX - boxWidth/2
+        spectators_start_y = mY - boxHeight/2
+    end
+
+    draw.Color( table.unpack(box1_color) ) -- drawing the darker box above the main box 
+    draw.FilledRect(math.floor(spectators_start_x), math.floor(spectators_start_y), math.floor(spectators_start_x + boxWidth), math.floor(spectators_start_y + 20))  
+
+    draw.Color( table.unpack(coloured_line) ) -- drawing the coloured line
+    draw.FilledRect(math.floor(spectators_start_x), math.floor(spectators_start_y) + 18, math.floor(spectators_start_x + boxWidth), math.floor(spectators_start_y + 20))
+
+    draw.SetFont( SpectatorFont ) -- drawing the keyboard logo
+    draw.Color( table.unpack(keyboard_logo_color) )
+    draw.Text(math.floor(spectators_start_x) + 2, math.floor(spectators_start_y) + 1,  "b" )
+    
+    draw.SetFont( deafultFont ) -- drawing the keybinds text
+    draw.Color(table.unpack(keybinds_color))
+    draw.Text(math.floor(spectators_start_x) + 25, math.floor(spectators_start_y) + 2, "spectators")
+
+--[[Here ends the spectators box]]--
 
 end
 end
